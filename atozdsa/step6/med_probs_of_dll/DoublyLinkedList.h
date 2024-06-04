432298798167f6bd42e7cac434a0229b97cec229
Akhilesh Sonone <akkhi.sonone@gmail.com>
2024-06-04 13:58:09 +0530

    add header file and functions for problems related to doubly linked list

---
+++ atozdsa/step6/med_probs_of_dll/DoublyLinkedList.h
@@ -0,0 +1,20 @@
+#ifndef DOUBLY_LINKED_LIST
+#define DOUBLY_LINKED_LIST
+
+#include <cstddef>
+#include <iostream>
+#include <vector>
+
+struct Node {
+  int data;
+  Node *next;
+  Node *previous;
+  Node(int d, Node *n = nullptr, Node *p = nullptr)
+      : data(d), next(n), previous(p) {}
+};
+std::vector<int> getInputsForDLL();
+Node *createDLL(std::vector<int> arr);
+void printDLL(Node *head);
+Node *reverseDLL(Node *head);
+
+#endif // !DOUBLY_LINKED_LIST
