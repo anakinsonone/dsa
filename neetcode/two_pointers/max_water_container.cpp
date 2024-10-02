e3f0a96e23df185113654a1c3363e16be5c1d151
anakinsonone <akkhi.sonone@gmail.com>
2024-10-02 16:29:18 +0530

    optimized solution for max water container

--- neetcode/two_pointers/max_water_container.cpp
+++ neetcode/two_pointers/max_water_container.cpp
@@ -17,6 +17,7 @@
  * Output: 4
 */
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -27,16 +28,27 @@ int main() {
   for (int i = 0; i < n; i++)
     std::cin >> heights[i];
 
-  int maxWater = 0;
+  int maxArea = 0;
   // brute force
-  for (int i = 0; i < heights.size(); i++) {
-    for (int j = i + 1; j < heights.size(); j++) {
-      int water = std::min(heights[i], heights[j]) * (j - i);
-      maxWater = std::max(maxWater, water);
-    }
+  // for (int i = 0; i < heights.size(); i++) {
+  //   for (int j = i + 1; j < heights.size(); j++) {
+  //     int water = std::min(heights[i], heights[j]) * (j - i);
+  //     maxWater = std::max(maxWater, water);
+  //   }
+  // }
+
+  // optimized
+  int left = 0, right = heights.size() - 1;
+  while (left < right) {
+    int area = (right - left) * (std::min(heights[left], heights[right]));
+    maxArea = std::max(maxArea, area);
+    if (heights[left] < heights[right])
+      left++;
+    else
+      right--;
   }
 
-  std::cout << maxWater;
+  std::cout << maxArea;
 
   return 0;
 }
