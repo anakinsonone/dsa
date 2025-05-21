154f6746b800ad0e4021a2e729e7b416f892e800
Akhilesh Sonone <akkhi.sonone@gmail.com>
2025-05-22 00:14:09 +0530

    finish up O(log n) solution for search in sorted rotated array

--- neetcode/binary_search/search_in_sort_rot_array.cpp
+++ neetcode/binary_search/search_in_sort_rot_array.cpp
@@ -32,15 +32,30 @@ int main() {
     std::cin >> nums[i];
   }
 
-  int res = -1;
   int left = 0, right = nums.size() - 1;
+  int res = -1;
   while (left <= right) {
-    int mid = left + (right - left) / 2;
+    int mid = (left + right) / 2;
     if (nums[mid] == target) {
       res = mid;
       break;
     }
+
+    if (nums[left] <= nums[mid]) {
+      if (target > nums[mid] || target < nums[left])
+        left = mid + 1;
+      else
+        right = mid - 1;
+    } else {
+      if (target < nums[mid] || target > nums[right]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
   }
+
+  std::cout << res;
 
   return 0;
 }
